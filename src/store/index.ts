2fcd1ef7cbeb52009d9281a1b3cc0a2fea78ba8b
John Williamson <johnjwilliamson@live.com>
2019-10-21 12:26:41 +1000

    remove redundant module

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import { general, GeneralModule } from './modules/general'
-import { bancor, BancorModule } from './modules/bancor'
 import { tokens, TokensModule } from './modules/tokens'
 import { convert, ConvertModule } from './modules/convert'
 import { eosTransit, EosTransitModule } from './modules/eosTransit'
@@ -15,7 +14,6 @@ Vue.use(Vuex)
 export const store = new Vuex.Store({
   modules: {
     general,
-    bancor,
     tokens,
     convert,
     eosTransit,
@@ -28,7 +26,6 @@ export const store = new Vuex.Store({
 
 export const vxm = {
   general: GeneralModule.CreateProxy(store, GeneralModule) as GeneralModule,
-  bancor: BancorModule.CreateProxy(store, BancorModule) as BancorModule,
   tokens: TokensModule.CreateProxy(store, TokensModule) as TokensModule,
   convert: ConvertModule.CreateProxy(store, ConvertModule) as ConvertModule,
   eosTransit: EosTransitModule.CreateProxy(
