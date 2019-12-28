88136f02223def975498cc8a1b48aba3bfbfd05c
John Williamson <johnjwilliamson@live.com>
2019-12-28 23:04:11 +1000

    support multiple networks

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,111 +1,145 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Wallet from '@/views/Wallet.vue'
-import WalletAccount from '@/views/WalletAccount.vue'
-import Tokens from '@/views/Tokens.vue'
-import Relays from '@/views/Relays.vue'
-import HeroConvert from '@/components/hero/sub/HeroConvert.vue'
-import HeroTransfer from '@/components/hero/sub/HeroTransfer.vue'
-import HeroRelay from '@/components/hero/sub/HeroRelay.vue'
-import HeroCreate from '@/components/hero/sub/HeroCreate.vue'
-import Navigation from '@/components/layout/Navigation.vue'
+import Vue from "vue";
+import Router, { RouteConfig } from "vue-router";
+import Wallet from "@/views/Wallet.vue";
+import WalletAccount from "@/views/WalletAccount.vue";
+import Tokens from "@/views/Tokens.vue";
+import Relays from "@/views/Relays.vue";
+import HeroConvert from "@/components/hero/sub/HeroConvert.vue";
+import HeroTransfer from "@/components/hero/sub/HeroTransfer.vue";
+import HeroRelay from "@/components/hero/sub/HeroRelay.vue";
+import HeroCreate from "@/components/hero/sub/HeroCreate.vue";
+import Navigation from "@/components/layout/Navigation.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
-  mode: 'history',
+const networkNamespaces = ["eos", "eth"];
+
+const commonRoutes = [
+  {
+    path: "",
+    name: "Tokens",
+    components: {
+      Nav: Navigation,
+      default: Tokens,
+      Hero: HeroConvert
+    }
+  },
+  {
+    path: "/transfer/:symbolName",
+    name: "Transfer",
+    components: {
+      Nav: Navigation,
+      default: Tokens,
+      Hero: HeroTransfer
+    },
+    props: true
+  },
+  {
+    path: "/relays",
+    name: "Relays",
+    components: {
+      Nav: Navigation,
+      default: Relays,
+      Hero: HeroRelay
+    }
+  },
+  {
+    path: "/relay/:account",
+    name: "Relay",
+    components: {
+      Nav: Navigation,
+      default: Relays,
+      Hero: HeroRelay
+    },
+    props: true
+  },
+  {
+    path: "/wallet",
+    name: "Wallet",
+    components: {
+      Nav: Navigation,
+      default: Wallet
+    }
+  },
+  {
+    path: "/wallet/:account",
+    name: "WalletAccount",
+    components: {
+      Nav: Navigation,
+      Hero: HeroTransfer,
+      default: WalletAccount
+    },
+    props: true
+  },
+  {
+    path: "/:symbolName",
+    name: "Token",
+    components: {
+      Nav: Navigation,
+      default: Tokens,
+      Hero: HeroConvert
+    },
+    props: true
+  }
+];
+
+//@ts-ignore
+const builtRoutes: RouteConfig[] = networkNamespaces
+  .map(network =>
+    commonRoutes.map(commonRoute => ({
+      ...commonRoute,
+      path: "/" + network + commonRoute.path,
+      name: `${network}-${commonRoute.name}`
+    }))
+  )
+  .flat(1);
+console.log(builtRoutes);
+
+export const router = new Router({
+  mode: "history",
   base: process.env.BASE_URL,
-  linkExactActiveClass: 'active',
+  linkExactActiveClass: "active",
   scrollBehavior(to, from, savedPosition) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         if (savedPosition) {
-          resolve(savedPosition)
+          resolve(savedPosition);
         } else {
-          resolve({ x: 0, y: 0 })
+          resolve({ x: 0, y: 0 });
         }
-      }, 500)
-    })
+      }, 500);
+    });
   },
   routes: [
     {
-      path: '*',
-      redirect: '/'
+      path: "*",
+      redirect: "/eos"
     },
     {
-      path: '/',
-      name: 'Tokens',
-      components: {
-        Nav: Navigation,
-        default: Tokens,
-        Hero: HeroConvert
-      }
+      path: "/",
+      redirect: "/eos"
     },
+    ...builtRoutes,
     {
-      path: '/transfer/:symbolName',
-      name: 'Transfer',
-      components: {
-        Nav: Navigation,
-        default: Tokens,
-        Hero: HeroTransfer
-      },
-      props: true
-    },
-    {
-      path: '/relays',
-      name: 'Relays',
-      components: {
-        Nav: Navigation,
-        default: Relays,
-        Hero: HeroRelay
-      }
-    },
-    {
-      path: '/relay/:account',
-      name: 'Relay',
-      components: {
-        Nav: Navigation,
-        default: Relays,
-        Hero: HeroRelay
-      },
-      props: true
-    },
-    {
-      path: '/wallet',
-      name: 'Wallet',
-      components: {
-        Nav: Navigation,
-        default: Wallet
-      }
-    },
-    {
-      path: '/wallet/:account',
-      name: 'WalletAccount',
-      components: {
-        Nav: Navigation,
-        Hero: HeroTransfer,
-        default: WalletAccount
-      },
-      props: true
-    },
-    {
-      path: '/relays/create',
-      name: 'Create',
+      path: "/eos/relays/create",
+      name: "Create",
       components: {
         Nav: Navigation,
         default: Relays,
         Hero: HeroCreate
       }
-    },
-    {
-      path: '/:symbolName',
-      name: 'Token',
-      components: {
-        Nav: Navigation,
-        default: Tokens,
-        Hero: HeroConvert
-      },
-      props: true
     }
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+  if (to && to.name && to.name.includes("-")) next();
+  else {
+    const originNetwork = from && from && from.name && from.name.split("-")[0];
+    if (originNetwork) {
+      next({ name: `${originNetwork}-${to.name}`, params: to.params });
+    } else {
+      next();
+    }
+  }
+});
