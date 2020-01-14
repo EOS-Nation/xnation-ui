efebd0df0b3a8743e4266e74305435fad5f6bc7d
John Williamson <johnjwilliamson@live.com>
2020-01-14 15:24:49 +1000

    f

--- src/components/hero/sub/HeroMerchant.vue
+++ src/components/hero/sub/HeroMerchant.vue
@@ -16,11 +16,11 @@
 
       <qrcode-vue :value="value" :size="150"></qrcode-vue>
 
-      <qrcode-stream @decode="onDecode" @init="onInit" />
+    <qrcode-stream @decode="onDecode" />
       <p class="error">{{ error }}</p>
 
       <p class="decode-result">
-        Last result: <b>{{ result }}</b>
+        Last result: <b>{{ result || "None." }}</b>
       </p>
     </div>
   </hero-wrapper>
@@ -51,31 +51,31 @@ export default class SortIcons extends Vue {
   }
 
   onDecode(result: string) {
-      alert(result);
+      console.log(result, 'was decode')
       this.result = result;
   }
 
-  async onInit(promise: any) {
-    try {
-      await promise;
-    } catch (error) {
-      if (error.name === "NotAllowedError") {
-        this.error = "ERROR: you need to grant camera access permisson";
-      } else if (error.name === "NotFoundError") {
-        this.error = "ERROR: no camera on this device";
-      } else if (error.name === "NotSupportedError") {
-        this.error = "ERROR: secure context required (HTTPS, localhost)";
-      } else if (error.name === "NotReadableError") {
-        this.error = "ERROR: is the camera already in use?";
-      } else if (error.name === "OverconstrainedError") {
-        this.error = "ERROR: installed cameras are not suitable";
-      } else if (error.name === "StreamApiNotSupportedError") {
-        this.error = "ERROR: Stream API is not supported in this browser";
-      } else {
-          this.error = error;
-      }
-    }
-  }
+//   async onInit(promise: any) {
+//     try {
+//       await promise;
+//     } catch (error) {
+//       if (error.name === "NotAllowedError") {
+//         this.error = "ERROR: you need to grant camera access permisson";
+//       } else if (error.name === "NotFoundError") {
+//         this.error = "ERROR: no camera on this device";
+//       } else if (error.name === "NotSupportedError") {
+//         this.error = "ERROR: secure context required (HTTPS, localhost)";
+//       } else if (error.name === "NotReadableError") {
+//         this.error = "ERROR: is the camera already in use?";
+//       } else if (error.name === "OverconstrainedError") {
+//         this.error = "ERROR: installed cameras are not suitable";
+//       } else if (error.name === "StreamApiNotSupportedError") {
+//         this.error = "ERROR: Stream API is not supported in this browser";
+//       } else {
+//           this.error = error;
+//       }
+//     }
+//   }
 }
 </script>
 
