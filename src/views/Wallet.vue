14fd8e6af0d924e0d4a2ddc88c811b9ec90ce5ec
John Williamson <johnjwilliamson@live.com>
2020-03-15 14:03:13 +1000

    remove junk

--- src/views/Wallet.vue
+++ src/views/Wallet.vue
@@ -6,10 +6,7 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import { vxm } from '@/store'
-import { bancorApi } from '@/api/bancor'
 import HeroWallet from '@/components/hero/HeroWallet.vue'
-import { TokenPrice } from '@/types/bancor'
 
 const numeral = require('numeral')
 
