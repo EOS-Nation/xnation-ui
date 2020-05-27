7d7211a00cf99c023660f5b6b7c42461ebb9c907
John Williamson <johnjwilliamson@live.com>
2020-05-27 15:11:33 +1000

    reformatted footer

--- src/components/common/MyFooter.vue
+++ src/components/common/MyFooter.vue
@@ -1,55 +1,51 @@
 <template>
   <b-container fluid>
-    <b-row class="footer">
-      <b-col>
+    <b-row align-v="center" class="footer">
+      <b-col cols="4">
         <a :href="website" target="_blank" class="orange">EOS NATION </a
         ><span class="text">© 2020</span>
       </b-col>
-      <b-col cols="10" md="6" class="d-flex">
-        <b-container>
-          <b-row class="d-flex justify-content-end">
-            <b-col sm="6" lg="2">
-              <router-link class="orange" :to="{ name: 'Privacy' }">
-                Privacy Policy
-              </router-link>
-            </b-col>
-            <b-col sm="6" lg="5">
-              <span class="text"
-                >Created with
-                <font-awesome-icon
-                  icon="heart"
+      <b-col cols="8" class="d-flex justify-content-end align-middle">
+        <ul class="list-inline ">
+          <li class="list-inline-item">
+            <router-link class="orange" :to="{ name: 'Privacy' }">
+              Privacy Policy
+            </router-link>
+          </li>
+          <li class="list-inline-item">
+            <span class="text"
+              >Created with
+              <font-awesome-icon
+                icon="heart"
+                fixed-width
+                :style="{ color: '#e04f1a' }"
+              />
+              by
+            </span>
+            <a :href="website" target="_blank" class="fas orange"
+              >EOS Nation
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <span class="text">Version: {{ appVersion }} </span>
+          </li>
+          <li class="list-inline-item">
+            <span class="text">
+              <a href="https://github.com/EOS-Nation/xnation-ui" target="_blank"
+                ><font-awesome-icon
+                  :style="{ color: 'white' }"
+                  :icon="['fab', 'github']"
                   fixed-width
-                  :style="{ color: '#e04f1a' }"
-                />
-                by
-              </span>
-              <a :href="website" target="_blank" class="fas orange"
-                >EOS Nation
-              </a>
-            </b-col>
-            <b-col sm="6" lg="2">
-              <span class="text">Version: {{ appVersion }} </span>
-            </b-col>
-            <b-col sm="6" lg="2">
-              <span class="text">
-                <a
-                  href="https://github.com/EOS-Nation/xnation-ui"
-                  target="_blank"
-                  ><font-awesome-icon
-                    :style="{ color: 'white' }"
-                    :icon="['fab', 'github']"
-                    fixed-width
-                /></a>
-                <a href="https://t.me/xnationio" target="_blank"
-                  ><font-awesome-icon
-                    :style="{ color: 'white' }"
-                    :icon="['fab', 'telegram']"
-                    fixed-width
-                /></a>
-              </span>
-            </b-col>
-          </b-row>
-        </b-container>
+              /></a>
+              <a href="https://t.me/xnationio" target="_blank"
+                ><font-awesome-icon
+                  :style="{ color: 'white' }"
+                  :icon="['fab', 'telegram']"
+                  fixed-width
+              /></a>
+            </span>
+          </li>
+        </ul>
       </b-col>
     </b-row>
   </b-container>
@@ -79,11 +75,18 @@ export default class MyFooter extends Vue {
   justify-content: space-between;
 }
 
-// @media (min-width: 768px) {
-//   .footer {
-//     padding: 15px 100px;
-//   }
-// }
+.list-inline {
+  margin: 0;
+}
+
+.list-inline-item {
+  border-right: 1px solid white;
+  padding-right: 6px;
+}
+
+.list-inline-item:last-child {
+  border-right: none;
+}
 
 .text {
   color: white;
