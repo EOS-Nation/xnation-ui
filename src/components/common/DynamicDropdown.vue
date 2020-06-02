c2111b65d7f27cd04f3962b3d3c91077f2a57e48
John Williamson <johnjwilliamson@live.com>
2020-06-02 16:34:35 +1000

    add colour variants

--- src/components/common/DynamicDropdown.vue
+++ src/components/common/DynamicDropdown.vue
@@ -4,7 +4,7 @@
       button
       :disabled="noMenus"
       @click="clicked"
-      :variant="noMenus ? 'warning' : 'success'"
+      :variant="variant"
       split
       class="m-2"
       size="lg"
@@ -30,12 +30,16 @@ import { Prop, Component, Vue, PropSync, Emit } from "vue-property-decorator";
 
 @Component
 export default class DynamicDropdown extends Vue {
-  @Prop(Array) menus!: [string, string, string][];
+  @Prop(Array) menus!: [string, string, string, string?][];
   @PropSync("selectedMenu", { type: String }) menu!: string;
   @Prop({ default: false }) disabled?: boolean;
 
   get icon() {
     return this.noMenus ? "question" : this.focusedMenu[2];
+  }
+
+  get variant() {
+    return this.noMenus ? 'warning' : this.focusedMenu[3] || 'success'
   }
 
   get title() {
