afbdb99aaba0cc3c02ffb6c5f132b8248aaac089
John Williamson <johnjwilliamson@live.com>
2020-07-04 17:36:40 +1000

    improved bump

--- bumpVersion.js
+++ bumpVersion.js
@@ -1,21 +1,17 @@
+// const arg1 = process.argv[2];
 const fs = require("fs");
 
-const packageJson = require("./package.json");
-
-const arg1 = process.argv[2];
+const raw = fs.readFileSync("./package.json", "utf8");
+const packageJson = JSON.parse(raw);
 
 const versionString = packageJson.version;
 
 const [major, minor, mini] = versionString.split(".");
-
 const lastVersion = String(Number(mini) + 1);
 
 const newVersionString = [major, minor, lastVersion].join(".");
 
-const newPackageJson = {
-  ...packageJson,
-  version: newVersionString
-};
+const newString = raw.replace(versionString, newVersionString);
 
-fs.writeFileSync("./package.json", JSON.stringify(newPackageJson, null, " "));
+fs.writeFileSync("./package.json", newString);
 console.log("Bumped package.json version to", newVersionString);
