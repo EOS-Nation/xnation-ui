4c461cf6d47fb8b4ac02b2e2bdbec73f86dc0a0a
Jan Langheimer <7262066+pingustar@users.noreply.github.com>
2020-07-27 11:58:29 +0200

    eslint fix

--- src/components/hero/sub/HeroRelay.vue
+++ src/components/hero/sub/HeroRelay.vue
@@ -19,6 +19,7 @@
       <template v-slot:liquidityActions>
         <b-row class="mb-4">
           <b-col sm="12">
+            {{ focusedRelay.v2 }}
             <b-btn
               :variant="
                 withdrawLiquidity ? (darkMode ? 'dark' : 'light') : 'primary'
@@ -42,11 +43,7 @@
       </template>
 
       <template slot="icon">
-        <font-awesome-icon
-          icon="plus"
-          class="text-primary"
-          size="1x"
-        />
+        <font-awesome-icon icon="plus" class="text-primary" size="1x" />
       </template>
 
       <div class="w-100">
@@ -123,11 +120,7 @@
             :tx-busy="txBusy"
           >
             <template slot="icon">
-              <font-awesome-icon
-                icon="plus"
-                class="text-primary"
-                size="1x"
-              />
+              <font-awesome-icon icon="plus" class="text-primary" size="1x" />
             </template>
             <template v-slot:footer>
               <b-col md="12" v-if="!(txBusy || success || error)">
@@ -136,7 +129,12 @@
                   :class="darkMode ? 'bg-body-dark' : 'bg-body-light'"
                 >
                   <div class="block-content py-2">
-                    <advanced-block-item v-for="item in advancedBlockItems" :key="item.label" :label="item.label" :value="item.value" />
+                    <advanced-block-item
+                      v-for="item in advancedBlockItems"
+                      :key="item.label"
+                      :label="item.label"
+                      :value="item.value"
+                    />
                   </div>
                 </div>
               </b-col>
@@ -147,7 +145,9 @@
                 :explorerLink="explorerLink"
                 :explorerName="explorerName"
                 @close="txModal = false"
-                :step-description="sections.length ? sections[stepIndex].description : undefined"
+                :step-description="
+                  sections.length ? sections[stepIndex].description : undefined
+                "
               />
               <span v-if="false">{{ sections[stepIndex].description }}</span>
               <b-col cols="12">
@@ -349,7 +349,7 @@ export default class HeroRelay extends Vue {
         label: "Share of Pool",
         value: "????"
       }
-    ]
+    ];
   }
 
   get token1Meta() {
