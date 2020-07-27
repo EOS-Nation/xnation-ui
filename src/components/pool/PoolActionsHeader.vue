265b66bb90c92b5c148df87e4b51bbe1ce3f26af
Jan Langheimer <7262066+pingustar@users.noreply.github.com>
2020-07-27 12:00:08 +0200

    create pool actions header comp

---
+++ src/components/pool/PoolActionsHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="d-flex justify-content-between w-100 align-items-center">
+    <font-awesome-icon
+      icon="chevron-left"
+      @click="$router.push({ name: 'Pool' })"
+      class="cursor"
+    />
+    <span>{{ withdrawLiquidity ? "Remove" : "Add" }} Liquidity</span>
+    <div class="d-flex align-items-center">
+      <b-badge variant="primary" class="px-2">{{ v2 ? "V2" : "V1" }}</b-badge>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import { vxm } from "@/store/";
+
+@Component({
+  components: {}
+})
+export default class PoolActionsHeader extends Vue {
+  @Prop() withdrawLiquidity!: boolean;
+  @Prop() v2!: boolean;
+
+  created() {}
+}
+</script>
+
+<style scoped lang="scss"></style>
