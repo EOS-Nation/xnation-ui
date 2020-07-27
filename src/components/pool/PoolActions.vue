14af2e8f039e00421768c13955c64e0a5aee8395
Jan Langheimer <7262066+pingustar@users.noreply.github.com>
2020-07-27 11:59:42 +0200

    create pool actions comp

---
+++ src/components/pool/PoolActions.vue
@@ -0,0 +1,38 @@
+<template>
+  <content-block>
+    <template slot="header">
+      <pool-actions-header
+        :withdraw-liquidity="withdrawLiquidity"
+        :v2="pool.v2"
+      />
+    </template>
+
+    <pool-actions-add v-if="!withdrawLiquidity" :pool="pool" />
+  </content-block>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { vxm } from "@/store/";
+import ContentBlock from "@/components/common/ContentBlock.vue";
+import { ViewRelay } from "@/types/bancor";
+import PoolActionsHeader from "@/components/pool/PoolActionsHeader.vue";
+import PoolActionsAdd from "@/components/pool/PoolActionsAdd.vue";
+
+@Component({
+  components: { PoolActionsAdd, PoolActionsHeader, ContentBlock }
+})
+export default class PoolActions extends Vue {
+  withdrawLiquidity = true;
+
+  get pool(): ViewRelay {
+    return vxm.bancor.relay(this.$route.params.account);
+  }
+
+  created() {
+    this.withdrawLiquidity = this.$route.params.poolAction === "remove";
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
