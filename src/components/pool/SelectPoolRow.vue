463f6885cb7466ff91d815693c683cc3bec0f1d5
Jan Langheimer <7262066+pingustar@users.noreply.github.com>
2020-07-27 12:00:42 +0200

    create select pool row comp for modal

---
+++ src/components/pool/SelectPoolRow.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="font-w600 font-size-14 d-flex align-items-center justify-content-between"
+    :class="darkMode ? 'text-dark' : 'text-light'"
+  >
+    <div>
+      <img
+        class="img-avatar img-avatar32 bg-white"
+        :src="pool.reserves[0].logo"
+        alt="Token Logo"
+      />
+      <img
+        class="img-avatar img-avatar32 bg-white overlap"
+        :src="pool.reserves[1].logo"
+        alt="Token Logo"
+      />
+      <span class="ml-2 mr-2">{{ pool.smartTokenSymbol }}</span>
+      <b-badge variant="primary" class="px-2">{{
+        pool.v2 ? "V2" : "V1"
+      }}</b-badge>
+    </div>
+    <span>~$??.??</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import { ViewRelay } from "@/types/bancor";
+import { vxm } from "@/store";
+
+@Component({
+  components: {}
+})
+export default class SelectPoolRow extends Vue {
+  @Prop() pool!: ViewRelay;
+
+  get darkMode() {
+    return vxm.general.darkMode;
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/_scss/custom/variables";
+
+.overlap {
+  margin-left: -10px;
+}
+</style>
